92051a4792313578ebc8b9288496467d5db016a8
Juan Perez <jperez69@hotmail.com>
2023-05-23 05:45:20 -0400

    Initial commit

--- ch02/app/src/stripping_names.py
+++ ch02/app/src/stripping_names.py
@@ -3,8 +3,7 @@ person_name = " albert eistein "
 print(person_name)
 print(person_name.lstrip())
 print(person_name.rstrip())
-person_name = " \talbert eistein "
-person_name = " \nalbert eistein "
+person_name = "\talbert eistein"
 print(person_name)
+person_name = "\nalbert eistein"
 print(person_name)
-
