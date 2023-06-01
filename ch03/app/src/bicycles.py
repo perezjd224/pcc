75cfc12f8d0bc3254b2a3a3406ff4171d5564450
Juan Perez <jperez69@hotmail.com>
2023-06-01 05:40:25 -0400

    TRY IT YOURSELF 3 - 2

---
+++ ch03/app/src/bicycles.py
@@ -0,0 +1,9 @@
+# List of bicycles manuafacturers
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(bicycles[0].title())
+print(bicycles[1].title())
+print(bicycles[3].title())
+print(bicycles[-1].title())
+print(message)
---
+++ ch03/app/src/try_it_yourself/friends.py
@@ -0,0 +1,13 @@
+# TRY IT YOURSELF 3 - 1
+names = ['albert einstein', 'aristocles son of ariston', 'stephen hawkings', 'brian greene']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+# TRY IT YOURSELF 3 - 2
+message = " ,you are insriational."
+print(names[0].title() + message)
+print(names[1].title() + message)
+print(names[2].title() + message)
+print(names[3].title() + message)
