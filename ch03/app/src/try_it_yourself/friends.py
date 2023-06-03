32cb6708e26ffadb2519621223f8fce4e5af012b
Juan Perez <jperez69@hotmail.com>
2023-06-03 05:57:28 -0400

    Intial commits

--- ch03/app/src/try_it_yourself/friends.py
+++ ch03/app/src/try_it_yourself/friends.py
@@ -11,3 +11,11 @@ print(names[0].title() + message)
 print(names[1].title() + message)
 print(names[2].title() + message)
 print(names[3].title() + message)
+
+# TRY IT YOURSELF 3 - 3
+motorcycles = ['harley', 'bmw', 'honda']
+message = "I would like to own a "
+print(message + motorcycles[0].capitalize()+".")
+print(message + motorcycles[1].upper()+".")
+print(message + motorcycles[2].capitalize()+".")
+
