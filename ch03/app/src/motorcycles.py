dadee8e7b6bb9a12bda62262881d70c5e2ddc31f
Juan Perez <jperez69@hotmail.com>
2023-06-07 10:07:43 -0400

    Intial commit

---
+++ ch03/app/src/motorcycles.py
@@ -0,0 +1,9 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+#motorcycles[0] = 'ducati'
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
