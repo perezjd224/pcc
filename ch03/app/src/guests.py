075e4d11dc00db72666b50a3ade8f18070146e9b
Juan Perez <jperez69@hotmail.com>
2023-06-08 06:01:45 -0400

    Intial commit.

---
+++ ch03/app/src/guests.py
@@ -0,0 +1,6 @@
+guest_list = ['albert enstein',
+              'aristocles son of ariston',
+              'stephen hawkings',
+              'brian greene']
+for name in guest_list:
+    print(name.title() + " has been invited to dinner.\n")
