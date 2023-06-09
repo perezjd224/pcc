cb1b1270df861c255f8512aa8ba6d1574d877444
Juan Perez <jperez69@hotmail.com>
2023-06-09 06:19:17 -0400

    updated guests.py

--- ch03/app/src/guests.py
+++ ch03/app/src/guests.py
@@ -1,6 +1,34 @@
+# 3-4 Guest List
 guest_list = ['albert enstein',
               'aristocles son of ariston',
               'stephen hawkings',
               'brian greene']
 for name in guest_list:
     print(name.title() + " has been invited to dinner.\n")
+
+# 3-5 Changing Guest List
+print(guest_list[2].title()+" is notable to attend.\n")
+guest_list[2] = 'sean carroll'
+for name in guest_list:
+    print(name.title() + " has been invited to dinner.\n")
+
+# 3-6 More Guest
+print("We have found a bigger table.\n")
+guest_list.insert(0,'aristotle')
+guest_list.insert(3,'michael angelo')
+guest_list.append('tal')
+for name in guest_list:
+    print(name.title() + " has been invited to dinner.\n")
+
+# 3-7 Shrinking Guest List
+print("There is only space for two guest.\n")
+print(guest_list.pop(6).title()+ "'s inviation has been cancelled.\n")
+print(guest_list.pop(5).title()+ "'s inviation has been cancelled.\n")
+print(guest_list.pop(4).title()+ "'s inviation has been cancelled.\n")
+print(guest_list.pop(3).title()+ "'s inviation has been cancelled.\n")
+print(guest_list.pop(2).title()+ "'s inviation has been cancelled.\n")
+for name in guest_list:
+    print(name.title() + " has been invited to dinner.\n")
+del guest_list[0]
+del guest_list[0]
+print(guest_list)
