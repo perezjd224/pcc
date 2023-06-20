e6b1e5dc3b5c58615d7917035f08cd129d77f147
Juan Perez <jperez69@hotmail.com>
2023-06-20 05:41:51 -0400

    file updated

---
+++ chess_project/app/src/data/classes/Square.py
@@ -0,0 +1,29 @@
+# /* Square.py
+import pygame
+
+
+# Tile creator
+class Square:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.abs_x = x * width
+        self.abs_y = y * height
+        self.abs_pos = (self.abs_x, self.abs_y)
+        self.pos = (x, y)
+        self.color = 'light' if (x + y) % 2 == 0 else 'dark'
+        self.draw_color = (220, 208, 194) if self.color == 'light' else (53, 53, 53)
+        self.highlight_color = (100, 249, 83) if self.color == 'light' else (0, 228, 10)
+        self.occupying_piece = None
+        self.coord = self.get_coord()
+        self.highlight = False
+        self.rect = pygame.Rect(
+            self.abs_x, 
+            self.abs_y,
+            self.width,
+            self.height
+        )
+
+        # get the formal notation of the title
