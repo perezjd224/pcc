623607645ae8bc4042976690babe73d54542259a
Juan Perez <jperez69@hotmail.com>
2023-06-22 07:06:05 -0400

    Intial commit

--- chess_project/app/src/animals.py
+++
@@ -1,7 +0,0 @@
-# TRY IT YOURSELF 4 - 2
-
-animals = ['eagle', 'falcon', 'owl']
-
-for bird in animals:
-    print(f"{bird.title()}s are birds of prey.\n")
-print("Birds of prey will not make good pets.")
--- chess_project/app/src/pizza.py
+++
@@ -1,7 +0,0 @@
-# TRY IT YOURSELF 4 - 1
-
-pizzas = ['cheese', 'pepperoni', 'supreme']
-
-for pizza in pizzas:
-    print(f"I like {pizza} pizza.\n")
-print("I like pizza!")
