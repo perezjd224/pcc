693520de20293e03d98224bba440c095a7548b03
Juan Perez <jperez69@hotmail.com>
2023-06-22 07:05:11 -0400

    Inital commit

---
+++ ch04/app/src/animals.py
@@ -0,0 +1,7 @@
+# TRY IT YOURSELF 4 - 2
+
+animals = ['eagle', 'falcon', 'owl']
+
+for bird in animals:
+    print(f"{bird.title()}s are birds of prey.\n")
+print("Birds of prey will not make good pets.")
---
+++ ch04/app/src/pizza.py
@@ -0,0 +1,7 @@
+# TRY IT YOURSELF 4 - 1
+
+pizzas = ['cheese', 'pepperoni', 'supreme']
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.\n")
+print("I like pizza!")
