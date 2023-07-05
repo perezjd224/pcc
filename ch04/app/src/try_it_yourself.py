c2876fa122e6f5cafc65a483578756eacd3ccb26
Juan Perez <jperez69@hotmail.com>
2023-07-05 06:27:16 -0400

    Updated file

--- ch04/app/src/try_it_yourself.py
+++ ch04/app/src/try_it_yourself.py
@@ -4,10 +4,35 @@ for x in range(1,21):
 
 
 # TRY IT YOUR 4 - 4
-
 numbers = []
 
 for number in range(1, 1000001):
     numbers.append(number)
+
 print(numbers)
-    
+
+# TRY IT YOUR 4 - 5
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+
+# TRY IT YOUR 4 - 6
+numbers = []
+
+for number in range(1, 20, 2):
+    numbers.append(number)
+
+print(numbers)
+
+# TRY IT YOUR 4 - 7
+numbers = []
+
+for number in range(1, 30, 3):
+    numbers.append(number)
+
+print(numbers)
+
+# TRY IT YOUR 4 - 8, 4-9
+cubes = [value**3 for value in range(1, 11)]
+  
+print(cubes) 
