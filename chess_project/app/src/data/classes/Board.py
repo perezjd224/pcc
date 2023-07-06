9130d0df410e3c489cd73b9e92c099eb266fd9db
Juan Perez <jperez69@hotmail.com>
2023-07-06 08:02:35 -0400

    Initial commit

---
+++ chess_project/app/src/data/classes/Board.py
@@ -0,0 +1,49 @@
+# /* board.py
+
+import pygame
+from data.classes.square import Square
+from data.classes.pieces.Rook import Rook
+from data.classes.pieces.Bishop import Bishop
+from data.classes.pieces.Knight import Knight
+from data.classes.pieces.Queen import Queen
+from data.classes.pieces.King import King
+from data.classes.pieces.Pawn import Pawn
+
+# Game state checker
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.tile_width = width // 8
+        self.tile_height = height // 8
+        self.selected_piece = None
+        self.turn = 'white'
+        self.config = [
+            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+            ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR'],
+        ]
+        self.squares = self.generate_squares()
+        self.setup_board()
+
+    def generate_squares(self):
+        output = []
+        for y in range(8):
+            for x in range(8):
+                output.append(
+                    Square(x,  y, self.tile_width, self.tile_height)
+                )
+        return output
+    
+    def get_square_from_pos(self, pos):
+        for square in self.squares:
+            if (square.x, square.y) == (pos[0], pos[1]):
+                return square
+
+    def get_piece_from_pos(self, pos):
+        return self.get_square_from_pos(pos).occupying_piece
