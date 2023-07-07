cf013c9ee6fd339ecee880e2fd2b0459a7f55e5e
Juan Perez <jperez69@hotmail.com>
2023-07-07 05:42:47 -0400

    Updated file

--- ch04/app/src/players.py
+++ ch04/app/src/players.py
@@ -1,2 +1,6 @@
 players = ['charles', 'martina', 'michael', 'florence', 'eli']
 print(players[-3:])
+
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
