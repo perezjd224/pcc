1b115bca0a57272ae279f1ec7554699e13723d55
Juan Perez <jperez69@hotmail.com>
2023-07-10 05:22:16 -0400

    Update file

---
+++ ch04/app/src/foods.py
@@ -0,0 +1,19 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friends_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friends_foods.append('ice cream')
+
+#print("My favorite foods are:")
+#print(my_foods)
+
+#print("\nMyfriend's favorite food's are:")
+#print(friends_foods)
+
+# TRY IT YOURSELF 4-10
+print("First three items of the list are:")
+print(my_foods[0:3])
+print("\nMiddle two items of the list are:")
+print(my_foods[1:3])
+print("\nLast three items of the list are:")
+print(my_foods[1:])
